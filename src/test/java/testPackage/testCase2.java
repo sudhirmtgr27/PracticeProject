390b8cba7faaac7204b8ca2936f23f08bc6cbe6a
Sudhir <sudhir.motghare27@gmail.com>
2022-03-31 17:57:57 +0530

    [sudhir] : 3nd commit added TC2 test case.

---
+++ src/test/java/testPackage/testCase2.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class testCase2
+{
+    @Test
+    public void TC2()
+    {
+        System.out.println("TC2 is passed...");
+
+    }
+
+}
